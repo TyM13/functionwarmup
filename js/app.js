04c024a23289e023695f7f2476889fa2c8a8593b
Your Name <you@example.com>
2022-07-14 16:46:43 -0600

    stuck on latest function need to work on it later

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// function strings (string_one){
+// console.log(string_one)
+// }
+
+// strings(`i like strings`)
+
+
+
+// function booleans (boo){
+//     if (boo < 1000) {
+//         console.log(`happy`)
+//     }else {
+//         console.log(`sad`)
+//     }
+// }
+// booleans(12234);
+
+
+// function numbers(number_one, number_two){
+//     if (number_one > number_two) {
+//         console.log(number_one)
+//         return number_one
+//     } else {
+//         if (number_one < number_two) {
+//             console.log(number_two)
+//             return number_two
+//         }
+//     }
+// }
+
+// numbers(434,349);
+
+
+
+function big_num(array_of_numbers) {
+    for (let counter = 0; counter < array_of_numbers.length; counter++) {
+        if (array_of_numbers[counter]) {
+
+        }else{
+            console.log(`error`)
+        }
+
+     
+}
+}
+
+big_num[(4, 7, 12, 45, 34, 3, 43)];
